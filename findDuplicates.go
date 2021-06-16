15ae1b8a4beded53fd75c70eba3a9d1b493d05ce
Matthias Heil <matthias@heil-privat.de>
2021-06-16 11:40:34 +0200

    restructure code, use flags to switch filemode and add documentation

--- findDuplicates.go
+++ findDuplicates.go
@@ -4,54 +4,81 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func main() {
+	filename := flag.String("file", "", "file to read from")
 	flag.Parse()
-	args := flag.Args()
 
-	var reader *bufio.Reader
-	if len(args) > 0 {
-		reader = bufio.NewReader(strings.NewReader(args[0] + "\n"))
+	var result map[string][]int
+	if *filename != "" {
+		result = collectLineOccurrencesInFile(*filename)
 	} else {
-		reader = bufio.NewReader(os.Stdin)
+		result = collectLineOccurrencesInStdin()
 	}
 
-	lineOccurences := findDuplicatedLines(reader)
+	printResult(result)
+}
 
-	for lineContent, occurances := range lineOccurences {
-		if len(occurances) < 2 {
+// Collects occurrences of lines in file with filename.
+// Returns map with line content as key and array of line numbers in which the line occurred as value.
+func collectLineOccurrencesInFile(filename string) map[string][]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return collectLineOccurrences(bufio.NewReader(file))
+}
+
+// Collects occurrences of lines in stdin.
+// Returns map with line content as key and array of line numbers in which the line occurred as value.
+func collectLineOccurrencesInStdin() map[string][]int {
+	return collectLineOccurrences(bufio.NewReader(os.Stdin))
+}
+
+// Prints the lineOccurrences which occurred more than one time.
+func printResult(lineOccurrences map[string][]int) {
+	for lineContent, occurrences := range lineOccurrences {
+		if len(occurrences) < 2 {
 			continue
 		}
 
-		fmt.Printf("line '%v' has been found in multiple lines: %v\n", lineContent, occurances)
+		fmt.Printf("line '%v' has been found in multiple lines: %v\n", lineContent, occurrences)
 	}
 }
 
-func findDuplicatedLines(reader *bufio.Reader) map[string][]int {
-	lineOccurences := make(map[string][]int)
+// Collects occurrences of lines provided by reader.
+// Returns map with line content as key and array of line numbers in which the line occurred as value.
+func collectLineOccurrences(reader *bufio.Reader) map[string][]int {
+	lineOccurrences := make(map[string][]int)
 
 	lineNumber := 1
 	for {
 		var lineContent, err = reader.ReadString('\n')
-		if err != nil {
+		if err == io.EOF {
 			break
+		}
+
+		if err != nil {
+			panic(err)
 		}
 
 		lineContent = strings.TrimSpace(lineContent)
 
-		var lineNumbers, isPresent = lineOccurences[lineContent]
+		var lineNumbers, isPresent = lineOccurrences[lineContent]
 		if !isPresent {
 			lineNumbers = make([]int, 0)
 		}
 
 		lineNumbers = append(lineNumbers, lineNumber)
-		lineOccurences[lineContent] = lineNumbers
+		lineOccurrences[lineContent] = lineNumbers
 
 		lineNumber++
 	}
 
-	return lineOccurences
+	return lineOccurrences
 }
